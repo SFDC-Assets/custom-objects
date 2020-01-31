2d70e1eb944b5658eca540105fb0cf8e8ff4d4ea
mshanemc <shane.mclaughlin@salesforce.com>
2020-01-31 13:44:47 -0600

    data loading

--- orgInit.sh
+++ orgInit.sh
@@ -2,3 +2,4 @@ sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
 sfdx force:source:push
 sfdx force:org:open
 sfdx force:user:permset:assign -n CustomObjectPerms
+sfdx automig:load -d data
