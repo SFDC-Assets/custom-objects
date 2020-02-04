81a597897c664ac60a7e8554e08e769ee3d2f233
mshanemc <shane.mclaughlin@salesforce.com>
2020-02-04 14:19:53 -0600

    fixes on init

--- orgInit.sh
+++ orgInit.sh
@@ -2,5 +2,5 @@ sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
 sfdx force:source:push
 sfdx force:org:open
 sfdx force:user:permset:assign -n CustomObjectPerms
-sfdx automig:load -d data
-sfdx force:user:password:set sfdx1234
+sfdx automig:load -d data --concise
+sfdx shane:user:password:set -p sfdx1234 -g User -l User
